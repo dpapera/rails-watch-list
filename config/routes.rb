8caa35aac9b22669a4160172b6f2c482feec76cf
Diego Papera <papera.diego@gmail.com>
2023-04-22 16:57:03 +0200

    added index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+  get 'lists/new'
+  get 'lists/create'
+  get 'lists/show'
+  get 'list/index'
+  get 'list/new'
+  get 'list/create'
+  get 'list/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
+  root 'lists#index'
+  resources :lists, only: [:index, :new, :create, :show]
 end
