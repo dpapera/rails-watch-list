8b06af2d1c790c84af65249a0987bca32743d741
Diego Papera <papera.diego@gmail.com>
2023-04-22 14:52:26 +0200

    validations added

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,4 +4,6 @@ class Bookmark < ApplicationRecord
 
   validates :movie_id, :list_id, presence: true
   validates :movie_id, uniqueness: { scope: :list_id, message: "is already in the list" }
+  validates :comment, length: { minimum: 6,
+    too_short: "the comment needs t be at least 6 characters long" }
 end
